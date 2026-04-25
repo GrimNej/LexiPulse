35c7703e178cdef36393d7208de830601a01c9f6
GrimNej <grim98413@gmail.com>
2026-04-25 23:38:48 +0545

    Add -k flag to curl for self-signed SSL cert

--- cron.sh
+++ cron.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-/usr/bin/curl -s -X POST "https://157.151.215.192/scheduler/run" \
+/usr/bin/curl -sk -X POST "https://157.151.215.192/scheduler/run" \
   -H "X-Scheduler-Key: 3bcee90aed169553459c1eae695828bfd00873585b8110a7b64490a74540da9c" \
   --max-time 120 --fail >> /home/ubuntu/LexiPulse/cron.log 2>&1
